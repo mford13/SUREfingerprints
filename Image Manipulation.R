478f336018bf22f862ed56b45d75b4c9bd865dbb
majorna98 <36868760+majorna98@users.noreply.github.com>
2018-06-06 21:46:42 -0400

    Add files via upload

--- Image Manipulation.R
+++ Image Manipulation.R
@@ -9,11 +9,38 @@ library(imager)
 library(ggplot2)
 library(plyr)
 library(dplyr)
-p1 = load.image('C:/Users/nmajo/Desktop/CMU SURE/PNGs/1.png')
-p1 # 4 color channels 
-plot(p1)
 
-p6 = load.image('C')
+# NEW CODE
+image.processing = function(directoryFrom = '', image.number = 0){
+  current.print = load.image(paste(directoryFrom, image.number, '.png', sep = ""))
+  df = as.data.frame(grayscale(current.print))
+  m = lm(value ~ x + y, data = df)
+  im.f = current.print-fitted(m)
+  
+  threshold(im.f,"13%") %>% plot
+  print ("Processing Complete")
+}
+
+save = function(plot, directoryTo = "", image.number = 0){
+  tiff(filename = paste(directoryTo, "/Updated", image.number, ".tiff", sep = ""))
+  print(plot)
+  dev.off()
+}
+
+image.cleanup = function(directoryFrom = '', image.number = 0, directoryTo = ''){
+  current.print = load.image(paste(directoryFrom, image.number, '.png', sep = ""))
+  df = as.data.frame(grayscale(current.print))
+  m = lm(value ~ x + y, data = df)
+  im.f = current.print-fitted(m)
+  print ("Processing Complete")
+  
+  save(threshold(grayscale(current.print),"13%") %>% plot, directoryTo, image.number)
+  print("Saved")
+}
+
+image.cleanup('C:/Users/nmajo/Dropbox/Fingerprints for SURE/Converted FingerprintsPNG/', 6, 'C:/Users/nmajo/Desktop')
+
+# END NEW CODE
 
 get.centers = function(im, thr = "99%"){
   dt = imhessian(im) %$% { xx*yy - xy^2 } %>% threshold(thr) %>% label
@@ -24,10 +51,10 @@ get.centers = function(im, thr = "99%"){
 # Compute determinant at scale "scale"
 hessdet = function(im, scale = 1) isoblur(im, scale) %>% imhessian %$% { scale^2*(xx*yy - xy^2) }
 # Note the scaling (scale^2) factor in the determinant
-plot(hessdet(hub, 1), main = "Determinant of the Hessian at Scale 1")
+#plot(hessdet(hub, 1), main = "Determinant of the Hessian at Scale 1")
 
 # function for individual photo
-# libraries needed to run function: 
+# libraries needed to run function: ggplot2, dplyr, plyr
 # parameters
 #   - directory: where the image is stored on your computer
 #   - image.number: the name of the file. Assumes the name is a number.
@@ -64,8 +91,8 @@ image.analysis = function(directory = '', image.number = 0){
   nblobs.denoised = isoblur(grad.mg,2)
   isoblur(grad.mg, 5) %>% get.centers("99.8%") %$% points(mx, my, col = "red")
   
-  dat = ldply(c(2,3,4), function(scale) hessdet(grad.mg, scale) %>% as.data.frame 
-              %>% mutate(scale=scale))
+  dat = ldply(c(2,3,4), function(scale) hessdet(grad.mg, scale) %>% as.data.frame %>% 
+                mutate(scale=scale))
   p = ggplot(dat,aes(x, y)) + 
     geom_raster(aes(fill = value))+
     facet_wrap(~ scale)+ 
@@ -80,15 +107,58 @@ image.analysis = function(directory = '', image.number = 0){
   i.max = llply(scales, function(scale) hessdet(grad.mg,scale)) %>% which.parmax
   plot(i.max, main = "Index of the Point-Wise Maximum \n Across Scales")
   
-  #Get a data.frame of labelled regions
+  # Get a data.frame of labelled regions
   labs = d.max %>% threshold("96%") %>% label %>% as.data.frame
-  #Add scale indices
+  # Add scale indices
   labs = mutate(labs, index = as.data.frame(i.max)$value)
   regs = dplyr::group_by(labs,value) %>% 
     dplyr::summarise(mx = mean(x), my = mean(y), scale.index = mean(index))
   
   print("Analysis complete")
-  
 }
 
-image.analysis('C:/Users/nmajo/Desktop/CMU SURE/PNGs/', 6)
+#image.analysis('C:/Users/nmajo/Desktop/CMU SURE/PNGs/', 6)
+image.analysis('C:/Users/nmajo/Dropbox/Fingerprints for SURE/Converted FingerprintsPNG/', 6)
+bdf = as.data.frame(p6)
+View(bdf)
+
+attach(bdf)
+lm = lm(value ~ x + y)
+summary(lm)
+
+# clean up images
+plot(p6)
+im.g = grayscale(p6)
+plot(im.g)
+df = as.data.frame(im.g)
+m = lm(value ~ x + y)
+threshold(im.g,"13%") %>% plot
+
+image.processing = function(directoryFrom = '', image.number = 0){
+  current.print = load.image(paste(directoryFrom, image.number, '.png', sep = ""))
+  df = as.data.frame(grayscale(current.print))
+  m = lm(value ~ x + y, data = df)
+  im.f = current.print-fitted(m)
+  
+  threshold(im.f,"13%") %>% plot
+  print ("Processing Complete")
+}
+
+save = function(plot, directoryTo = "", image.number = 0){
+  tiff(filename = paste(directoryTo, "/Updated", image.number, ".tiff", sep = ""))
+  print(plot)
+  dev.off()
+}
+
+image.cleanup = function(directoryFrom = '', image.number = 0, directoryTo = ''){
+  current.print = load.image(paste(directoryFrom, image.number, '.png', sep = ""))
+  df = as.data.frame(grayscale(current.print))
+  m = lm(value ~ x + y, data = df)
+  im.f = current.print-fitted(m)
+  print ("Processing Complete")
+  
+  save(threshold(grayscale(current.print),"13%") %>% plot, directoryTo, image.number)
+  print("Saved")
+}
+
+image.cleanup('C:/Users/nmajo/Dropbox/Fingerprints for SURE/Converted FingerprintsPNG/', 6, 'C:/Users/nmajo/Desktop')
