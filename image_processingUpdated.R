c051855fdb3ec067a90224ba0edd82f93d39fa17
majorna98 <36868760+majorna98@users.noreply.github.com>
2018-07-09 14:37:14 -0400

    Add files via upload

--- image_processingUpdated.R
+++ image_processingUpdated.R
@@ -18,7 +18,9 @@ source("image_functionsUpdated.R")
 #win_size = 5
 #stride = 5
 img_featurized_updated = 
-  func_gen_img_qual(280, dir_ = "C:/Users/nmajo/Dropbox/Fingerprints for SURE/Processed/", 
+  func_gen_img_qual(280, dir_ = "C:/Users/nmajo/Dropbox/Fingerprints for SURE/Processed (480 x 480)/", 
                     win_size = 100, stride = 10)
+print(img_featurized_updated)
+
 setwd("C:/Users/nmajo/Desktop/CMU SURE")
 save(img_featurized_updated, file = "image_featurized_updated")
